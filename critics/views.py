af6a7f1ddc0e0aeef948fb4d3ca72a0ba01055be
manifairid03 <manifaridi03@gmail.com>
2024-03-02 18:42:42 +0330

    done baby

--- critics/views.py
+++ critics/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
 from .models import User, Critic
 from .forms import CriticForm, RegisterForm, LoginForm
 
@@ -23,19 +24,60 @@ def createc(request):
         return redirect('critics:login')
 
 def readc(request, id):
-    pass
+    c = Critic.objects.get(id=id)
+    return render(request, 'read.html', {'object':c})
 
 def listc(request):
-    pass
+    cqs = Critic.objects.all()
+    render(request, 'list.html', {'objs':cqs})
 
 def deletec(request, id):
-    pass
+    c = Critic.objects.get(id=id)
+    if request.user.id == c.creator.id:
+        c.delete()
+        return redirect('critics:create')
 
 def usersignup(request):
-    pass
+    if request.user.is_authenticated:
+        return redirect('critics:logout')
+    else:
+        if request.method == 'POST':
+            form = RegisterForm(request.POST)
+            if form.is_valid():
+                user = User.objects.create_user(
+                    username=form.cleaned_data['username'],
+                    email=form.cleaned_data['email'],
+                    password=form.cleaned_data['password'],
+                )
+                login(request, user)
+                return redirect('critics:all-c')
+            else:
+                return render(request, 'create.html', {'form':form})
+        else:
+            return render(request, 'create.html', {'form':RegisterForm()})
 
 def userlogin(request):
-    pass
+    if request.user.is_authenticated:
+        return redirect('critics:logout')
+    else:
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                user = authenticate(
+                    username=form.cleaned_data['username'],
+                    password=form.cleaned_data['password']
+                )
+                if user:
+                    login(request, user)
+                    return redirect('critics:all-c')
+                else:
+                    return render(request, 'create.html', {'form':form})
+
+            else:
+                return render(request, 'create.html', {'form':form})
+        else:
+            return render(request, 'create.html', {'form':LoginForm()})
 
 def userlogout(request):
-    pass
+    logout(request)
+    return redirect('critics:login')
